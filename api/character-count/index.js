f4cdf9489d79c75146776463e500b70283db421d
AL AMIN KOUSER <alaminkouser@icloud.com>
2024-04-17 16:38:13 +0600

    Added character count API endpoint

---
+++ api/character-count/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+/**
+ * Character Count API
+ * The API will return the number of characters in a text
+ * The API body must be a JSON object with a key "text" and a value of type "string"
+ * Example: { "text": "Hello World" }
+ * The API will return a JSON object with a key "count" and a value of type "number"
+ * Example: { "count": 11 }
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ */
+export const characterCount = (req, res) => {
+    console.log("Request received");
+    if (req.method !== "POST") {
+        return res.status(405).send("Method Not Allowed");
+    }
+    if (req.headers["content-type"] !== "application/json") {
+        return res.status(400).send("Bad Request");
+    }
+    if (!req.body || !req.body.text) {
+        return res.status(400).send("Bad Request");
+    }
+    return res.json({ count: req.body.text.length });
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { wordCount } from "./api/word-count/index.js";
+import { characterCount } from "./api/character-count/index.js";
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,11 @@ app.use(express.json());
 app.route("/")
     .get((_req, res) => {
         res.send("Hello WSD!");
+    });
+
+app.route("/api/character-count/")
+    .post((req, res) => {
+        characterCount(req, res);
     });
 
 app.route("/api/word-count/")
