66867922b00de92d02115d5c1bd1124e9722699c
AL AMIN KOUSER <alaminkouser@icloud.com>
2024-04-17 17:01:30 +0600

    Added sentence count API endpoint

---
+++ api/sentence-count/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+/**
+ * Sentence Count API
+ * The API will return the number of sentences in a text
+ * The API body must be a JSON object with a key "text" and a value of type "string"
+ * Example: { "text": "Hello World. How are you?" }
+ * The API will return a JSON object with a key "count" and a value of type "number"
+ * Example: { "count": 2 }
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+export const sentenceCount = (req, res) => {
+    console.log("Request received");
+    if (req.method !== "POST") {
+        return res.status(405).send("Method Not Allowed");
+    }
+    if (req.headers["content-type"] !== "application/json") {
+        return res.status(400).send("Bad Request");
+    }
+    if (!req.body || !req.body.text) {
+        return res.status(400).send("Bad Request");
+    }
+    let count = 0;
+    let text = req.body.text;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] === "." || text[i] === "?" || text[i] === "!") {
+            count++;
+        }
+    }
+    return res.json({ count: count });
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { wordCount } from "./api/word-count/index.js";
 import { characterCount } from "./api/character-count/index.js";
+import { sentenceCount } from "./api/sentence-count/index.js";
+import { wordCount } from "./api/word-count/index.js";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,11 @@ app.route("/")
 app.route("/api/character-count/")
     .post((req, res) => {
         characterCount(req, res);
+    });
+
+app.route("/api/sentence-count/")
+    .post((req, res) => {
+        sentenceCount(req, res);
     });
 
 app.route("/api/word-count/")
