6b32f87ce3a8f669faa945784eaa9b915de93117
AL AMIN KOUSER <alaminkouser@icloud.com>
2024-04-17 16:28:19 +0600

    added: Word Count API

---
+++ api/word-count/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+/**
+ * Word Count API
+ * The API will return the number of words in a text
+ * The API body must be a JSON object with a key "text" and a value of type "string"
+ * Example: { "text": "Hello World" }
+ * The API will return a JSON object with a key "count" and a value of type "number"
+ * Example: { "count": 2 }
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ */
+export const wordCount = (req, res) => {
+    console.log("Request received");
+    if (req.method !== "POST") {
+        return res.status(405).send("Method Not Allowed");
+    }
+    if (req.headers["content-type"] !== "application/json") {
+        return res.status(400).send("Bad Request");
+    }
+    if (!req.body || !req.body.text) {
+        return res.status(400).send("Bad Request");
+    }
+    const words = req.body.text.split(" ");
+    return res.json({ count: words.length });
+}
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from "express";
+import { wordCount } from "./api/word-count/index.js";
 
 const app = express();
+app.use(express.json());
 
 app.route("/")
     .get((_req, res) => {
         res.send("Hello WSD!");
+    });
+
+app.route("/api/word-count/")
+    .post((req, res) => {
+        wordCount(req, res);
     });
 
 app.listen(3000, () => {
